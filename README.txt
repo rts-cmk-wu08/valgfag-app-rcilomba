17ccce74b57882cc4511f4d99665365d800969ea
Ramadan Cilomba <rcilomba@yahoo.com>
2023-08-31 10:17:31 +0200

    Add existing project files to Git

---
+++ README.txt
@@ -0,0 +1,12 @@
+Formålet med denne applikation er at kunne give brugeren relevant vejrinformation i den by, brugeren leder efter information om. Med denne vejr-app kan brugeren se temperatur, dato, by og meteorologi.
+En bruger kan også installere appen på deres enheders startskærme, når den er bygget som en PWA, hvilket giver en app-lignende oplevelse samt er i stand til at bruge den offline.
+
+Det gode ved en vejr-app er, at den har hurtig adgang til vejrinformation for specifikke byer, i dette tilfælde er det for den pågældende dag. En vejr-app er også praktisk til rejser, når du gerne vil vide, hvad graderne er i den by. Med PWA er appen god, hvis du skal vide, hvordan vejret kommer til at se ud, selvom du ikke har adgang til internettet.
+
+Applikationen er nyttig for en, der ønsker at vide, hvad det aktuelle vejr er for en bestemt by.
+
+Appens vigtigste funktion er, at du kan se vejret for den aktuelle by, altså dens grader og meteorologi. Samt kan du også se dagens dato for vejret og hvilken by det gælder for.
+
+I dette tilfælde er det ikke nødvendigt med en router, da denne app er en enkeltsideapplikation, men heller ikke har en navigation.
+
+Hvis jeg havde mere tid, ville jeg gerne have tilføjet vejr for 5 plus dage og ikke kun en dag for det aktuelle tidspunkt. Samt tilføje meteorologiikon.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import OneSignal from 'react-onesignal';
+import PWAPrompt from 'react-ios-pwa-prompt';
+
+const api = {
+  key: "61ae3c88e444a235897bb4086e509977",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  useEffect(() => {
+    OneSignal.init({appId: process.env.REACT_APP_ONESIGNAL });
+  }, []);
+  
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showLocationBox, setShowLocationBox] = useState(false);
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      if (!query) {
+        setErrorMessage('Please enter a City!');
+        setShowLocationBox(false);
+      } else {
+        fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
+          .then(res => res.json())
+          .then(result => {
+            setWeather(result);
+            setQuery('');
+            setErrorMessage('');
+            setShowLocationBox(true);
+            console.log(result)
+          });
+      }
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "Mars", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  let className = 'app';
+
+  if (typeof weather.main !== "undefined") {
+    if (weather.main.temp > 28) {
+      className = 'app hot';
+    } else if (weather.main.temp > 16) {
+      className = 'app warm';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className={className}>
+      <main>
+        <div className="search-box">
+          <input type="text"
+            className="search-bar"
+            placeholder="Enter a City..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {errorMessage && (
+          <div className="error-message">{errorMessage}</div>
+        )}
+        {showLocationBox && typeof weather.main !== "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°c
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : null}
+      </main>
     </div>
+    <PWAPrompt copyTitle="Add to homescreen" />
+    </>
   );
 }
 
 export default App;
+
+
+  
+
